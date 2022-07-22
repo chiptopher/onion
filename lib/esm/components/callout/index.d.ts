97f79e3e40b443f677bde345e5613e04fcf3a87e
chiptopher <chrisboyerdev@gmail.com>
2022-07-22 10:18:11 -0400

    Build 0.1.9

--- lib/esm/components/callout/index.d.ts
+++ lib/esm/components/callout/index.d.ts
@@ -2,7 +2,7 @@ import React from 'react';
 import { Colors } from '../../atoms/colors';
 interface Props {
     children: React.ReactNode;
-    color: Exclude<Colors, 'light' | 'text'>;
+    color: Exclude<Colors, 'text'>;
 }
 export declare const Callout: React.FunctionComponent<Props>;
 export {};
