45f8d08575d9a720b19b34477033c5e36fb6bfb4
chiptopher <chrisboyerdev@gmail.com>
2022-07-23 09:00:34 -0400

    Rebuild

--- lib/esm/atoms/util.d.ts
+++ lib/esm/atoms/util.d.ts
@@ -3,5 +3,5 @@ export declare type Mapping<T extends string, R = string> = {
     [K in T]: R;
 };
 export interface ChildrenOnlyProps {
-    children: React.ReactNode;
+    children?: React.ReactNode;
 }
