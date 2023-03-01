2092d0d17ea668f08be048ec84f1e207acd650b5
chiptopher <chrisboyerdev@gmail.com>
2023-03-01 10:34:48 -0500

    Increase margin between last form element and Form.Buttons

--- src/form/buttons.tsx
+++ src/form/buttons.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Block } from '../atoms/block';
 import { Button } from '../blocks/button';
 import { Loading } from '../blocks/loading';
 import { Buttons } from '../components/buttons';
@@ -7,12 +8,14 @@ import { Buttons } from '../components/buttons';
 interface Props {}
 
 export const FormButtons: React.FunctionComponent<Props> = () => (
-    <Buttons>
-        <Button type="reset">Cancel</Button>
-        <Button type="submit">
-            <Loading.Spinner>Submit</Loading.Spinner>
-        </Button>
-    </Buttons>
+    <Block marginTop="1.5">
+        <Buttons>
+            <Button type="reset">Cancel</Button>
+            <Button type="submit">
+                <Loading.Spinner>Submit</Loading.Spinner>
+            </Button>
+        </Buttons>
+    </Block>
 );
 
 FormButtons.displayName = 'Form.Buttons';
