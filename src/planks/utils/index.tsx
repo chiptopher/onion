83906139b77bab21aefd303dd2ad179a4d3b606a
chiptopher <chrisboyerdev@gmail.com>
2023-03-08 12:53:28 -0500

    Give plank wrappers a trimmedTop property that reduces the paddingTop

--- src/planks/utils/index.tsx
+++ src/planks/utils/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import { Block } from '../../atoms/block';
+import { SpacingValues } from '../../atoms/block/types';
+import { BlockSpacing } from '../../atoms/block/util';
 import { Colors, Tint } from '../../atoms/colors';
 
 export interface PlankWrapperProps {
     background?: Colors;
     backgroundTint?: Tint;
+    trimmedTop?: Extract<SpacingValues, BlockSpacing>;
 }
 
 interface _Props {
@@ -18,13 +21,15 @@ export const PlankWrapper: React.FunctionComponent<Props> = ({
     children,
     backgroundTint,
     background,
+    trimmedTop,
 }) => {
     return (
         <Block
+            className="onion-plank-wrapper"
             color={background}
             colorTint={backgroundTint}
             paddingBottom="6"
-            paddingTop="6"
+            paddingTop={trimmedTop || '6'}
         >
             {children}
         </Block>
