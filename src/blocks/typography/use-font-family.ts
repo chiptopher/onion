0fe5bbdb86b4418f3c749c25843d4ed55b0bf93b
chiptopher <chrisboyerdev@gmail.com>
2023-03-19 12:34:36 -0400

    Refactor useFontFamily

--- src/blocks/typography/use-font-family.ts
+++ src/blocks/typography/use-font-family.ts
@@ -1,39 +1,31 @@
 import { useTheme } from '../../theme';
 import { TextStlye } from './text';
 
+type StyleMapping = {
+    [K in TextStlye]: string;
+};
+
+const styleMapping: StyleMapping = {
+    body: 'body',
+    caption: 'caption',
+    header: 'heading',
+    label: 'label',
+    'larger-body': 'largerBody',
+    subheader: 'subheader',
+    title: 'title',
+};
+
 export function useFontFamily(
     given: string | undefined,
     style: TextStlye
 ): string {
-    if (given) return given;
     const theme = useTheme();
     const typographyOverrides = theme.components?.typography;
-    if (typographyOverrides) {
-        const { body, caption, heading, label, largerBody, subheader, title } =
-            typographyOverrides;
-        switch (style) {
-            case 'title':
-                if (title?.fontFamily) return title.fontFamily;
-                break;
-            case 'caption':
-                if (caption?.fontFamily) return caption.fontFamily;
-                break;
-            case 'body':
-                if (body?.fontFamily) return body.fontFamily;
-                break;
-            case 'label':
-                if (label?.fontFamily) return label.fontFamily;
-                break;
-            case 'subheader':
-                if (subheader?.fontFamily) return subheader.fontFamily;
-                break;
-            case 'header':
-                if (heading?.fontFamily) return heading.fontFamily;
-                break;
-            case 'larger-body':
-                if (largerBody?.fontFamily) return largerBody.fontFamily;
-                break;
-        }
-    }
-    return "'Nunito', sans-serif";
+    const fontFamily =
+        given ||
+        (typographyOverrides &&
+            typographyOverrides[styleMapping[style]]?.fontFamily) ||
+        "'Nunito', sans-serif";
+
+    return fontFamily;
 }
