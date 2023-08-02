973c158cced7f2bf631ca4374afa95db6b777ba8
chiptopher <chrisboyerdev@gmail.com>
2023-08-02 09:19:42 -0400

    Fix 'npm install' errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import postcssEach from 'postcss-each';
 import postcssImport from 'postcss-import';
 import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import { terser } from 'rollup-plugin-terser';
 
 // eslint-disable-next-line
 const packageJson = require('./package.json');
